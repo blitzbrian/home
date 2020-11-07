f976e95e64e3a912507c519680c72bdaeb5e1710
baliomaker <59792880+baliomaker@users.noreply.github.com>
2020-11-07 16:30:01 +0100

    Update background.js

--- background.js
+++ background.js
@@ -21,18 +21,18 @@ function setup () {
 
 function draw () {
   background('#2d2c3e');
-  if(logos[i].y < window.innerHeight) {
     push();
     for(let i = 0; i < logos.length; i++) {
-      image(logo,logos[i].x,logos[i].y,50,50);
-      logos[i].y++;
-    }
-    pop();
-  } else {
+      if(logos[i].y < window.innerHeight) {
+        image(logo,logos[i].x,logos[i].y,50,50);
+        logos[i].y++;
+    } else {
     logos.splice(i,1);
     logos.push({
       x: random(window.innerWidth),
       y: -10
     });
   }
+   }
+   pop();
 }
