3860e9aa41cdd41947b72429793a5a00329e79ed
baliomaker <59792880+baliomaker@users.noreply.github.com>
2020-11-07 13:57:03 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 let canvas;
 let logo;
+let logos = [];
+
 
 function preload () {
   logo = loadImage('baliomaker.png')
@@ -9,11 +11,23 @@ function setup () {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.position(0,0);
   canvas.style('z-index:-1;');
+  angleMode(DEGREES);
+  for(let i = 0; i < 10; i++) {
+    logos[i] = {
+      x: random(window.innerWidth),
+      y: 0,
+      r: random(180)
+    }
+  }
 }
 
 function draw () {
   background('#2d2c3e');
-  fill(10);
-  rect(mouseX-25,mouseY-25,50,50,25);
-  image(logo,mouseX-25,mouseY-25,50,50);
+  for(let i = 0; i < logo.length; i++) {
+    clogo = logos[i]
+    rotate(clogo.r);
+    image(logo,clogo.x,clogo.y,50,50);
+    logos[i].y++;
+    logos[i].r++;
+  }
 }
