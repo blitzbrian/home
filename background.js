beaa17dc1ce8bb8b70d779b8d657f9021a85c5dd
baliomaker <59792880+baliomaker@users.noreply.github.com>
2020-11-07 17:45:26 +0100

    Update background.js

--- background.js
+++ background.js
@@ -9,12 +9,12 @@ function preload () {
 
 function setup () {
   canvas = createCanvas(0,0);
-  canvas.position(0,0);
+  canvas.position(document.body.scrollWidth,document.body.scrollHeight);
   canvas.style('z-index:-1;');
-  for(let i = 0; i < (window.innerHeight/50); i++) {
+  for(let i = 0; i < (document.body.scrollHeight/50); i++) {
     logos[i] = {
-      x: random(window.innerWidth),
-      y: random(-1000 + (window.innerHeight/1.5),-10)
+      x: random(document.body.scrollHeight),
+      y: random(-1000 + (document.body.scrollHeight/1.5),-10)
     }
   }
 }
@@ -23,14 +23,14 @@ function draw () {
   background('#2d2c3e');
     push();
     for(let i = 0; i < logos.length; i++) {
-      if(logos[i].y < window.innerHeight) {
+      if(logos[i].y < document.body.scrollHeight) {
         image(logo,logos[i].x,logos[i].y,50,50);
         logos[i].y++;
     } else {
     logos.splice(i,1);
     logos.push({
-      x: random(window.innerWidth),
-      y: random(-1000 + (window.innerHeight/1.5),-10)
+      x: random(document.body.scrollHeight),
+      y: random(-1000 + (document.body.scrollHeight/1.5),-10)
     });
   }
    }
@@ -38,8 +38,8 @@ function draw () {
 }
 
 window.onresize = function() {
-  var w = window.innerWidth;
-  var h = window.innerHeight;  
+  var w = document.body.scrollWidth;
+  var h = document.body.scrollHeight;  
   canvas.size(w,h);
   width = w;
   height = h;
