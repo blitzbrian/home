2a54e81c9751d723ebc5e1e70a79208e6bf798b7
baliomaker <59792880+baliomaker@users.noreply.github.com>
2020-11-08 12:36:14 +0100

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -1,1 +1,1 @@
-gsap.from('.container',{duration: 1,y:'-100%',opacity:0,stagger:0.5,pointerEvents:none, ease: "back.inOut(3)"});
+gsap.from('.container',{duration: 1,y:'-100%',opacity:0,stagger:0.5,pointerEvents:"none", ease: "back.inOut(3)"});
