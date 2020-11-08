779f249781f8a1a6a980cc5f13ae30715ada9818
baliomaker <59792880+baliomaker@users.noreply.github.com>
2020-11-08 11:22:43 +0100

    Update gsap.js

--- gsap.js
+++ gsap.js
@@ -1,1 +1,2 @@
-gsap.from('.container',{duration: 1,y:'-100vh',opacity:0,stagger:0.5})
+gsap.from('.container',{duration: 1,y:'-100%',opacity:0,stagger:0.5});
+gsap.from('canvas',{darution:2,opacity:0,delay:2});
