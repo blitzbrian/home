4ce2cde53f7c2a65e959293a72d9d0d64e316ae2
Dazer Studios <59792880+dazerstudio@users.noreply.github.com>
2023-01-06 16:25:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
-    outDir: "/"
+    outDir: "build"
   },
   server: {
     strictPort: true,
